39d1fe094df9768921e98e2c3e025780c3f2c69a
Warwick Allen <warwick@remarque.co.nz>
2025-08-24 13:09:21 +1200

    Add button to swap names
    In the HTML output, include a button to swap between the Hebrew- and Greek-based
    names.

--- tex2html.pl
+++ tex2html.pl
@@ -4,6 +4,66 @@ use warnings;
 use strict;
 use Try::Tiny;
 
+sub getBib {
+  local $_;
+  my ($fh, %items, $key, $order, @html);
+  open $fh, shift or die $!;
+  while (<$fh>) {
+    /\@(.\w+)\{(\w+),/ and do {
+      $key = $2;
+      $items{$key} = {type => $1, order => ++$order};
+      next };
+    /(\w+)\s*=\s*\{(.*)\}/ and do {
+      my ($field, $value) = ($1, $2);
+      $value =~ /^\{(.*)\}$/ and $value = $1;
+      $items{$key}{$field} = $value;
+    }
+  }
+  close $fh;
+
+  my @fields = qw/
+    author
+    title
+    booktitle
+    journal
+    series
+    pages
+    editor
+    translator
+    volume
+    number
+    publisher
+    address
+    year
+    note
+  /;
+  return join '', map "$_\n",
+    '  <div id="bibliography">',
+    '    <h2>Bibliography</h2>',
+    grep {length} map({
+      my $item = $items{$_} or next;
+      qq{    <div id="$_">},
+      join('', grep {defined} map {
+        $item->{$_} and do {
+          my $field = $_;
+          $field =~ s/(\w)/\U$1/;
+          $field =~ s/kt/k T/;
+          my $value = $item->{$_};
+          $value =~ s/--/&ndash;/g;
+          <<HERE}
+      <div class="bib_$_">
+        <div class="field">$field</div>
+        <div class="value">$value</div>
+      </div>
+HERE
+      } @fields).
+      qq{    </div><!-- end $_ -->},
+    } sort {$items{$a}{order} <=> $items{$b}{order}} keys %items),
+    '  </div><!-- end bibliograhpy -->'
+}
+
+my $bib = getBib('references.bib');
+
 my $in = shift;
 (my $base) = $in =~ /(.*)(?=\.)/;
 my $out = "$base.html";
@@ -52,26 +112,52 @@ my $style = <<HERE;
    margin-top: 2ex;
    margin-bottom: 1ex;
  }
+
+ #bibliography > div:first-of-type {
+   border-top: 1px solid grey;
+ }
+ #bibliography > div {
+   border-bottom: 1px solid grey;
+   padding: 1ex 0;
+ }
+ #bibliography > div > div > div {
+   display:inline-block;
+   line-height: 2.5ex;
+ }
+ #bibliography .field {
+   color:grey;
+   font-size: 75%;
+   %font-variant-caps: small-caps;
+   min-width: 8em;
+   text-align: right;
+ }
 </style>
 HERE
 try {
   system "cp '$in' '$tmp.tex'" and die $!;
-  system "make4ht -x '$tmp.tex'" and die $!;
+  system "make4ht -e $base.mk4 -x '$tmp.tex'" and die $!;
 
   my $ref_num;
   open IN, "$tmp.html" or die $!;
   open OUT, '>', $out or die $!;
   select OUT;
-  sub strip {
-    local $_ = $1;
-    s/$_[0]//g;
-    $_;
+  sub getRef {
+    our %div;
+    my $fname = shift;
+    open FH, $fname or die "Cannot open '$fname': $!";
+    undef local $/;
+    local $_ = <FH>;
+    close FH;
+    m~(<div\b.*?/div>)~s or die "Cannot find div in '$fname'";
+    $_ = $1;
+    /class='ec-lmbx-10'>(.*?)</ && $1
   }
   $/ = ">";
   while (<IN>) {
-    s~ (?=</head>)                      ~$style\n~x                         or
-    s~ (?<=<body>)                      ~\n<div class="block">~x            or
-    s~ (?=</body>)                      ~</div><!-- class="block" -->\n~x   or
+    s~ (?=</head>) ~$style\n~x                                      or
+    s~ (?<=<body>) ~\n<div class="block">~x                         or
+    s~ ($tmp\d+\.html)\#\w+ ~my $r = getRef $1; "#$r' title='$r"~xe or
+    s~ (?=</body>) ~$bib</div><!-- class="block" -->\n~x          or
     1;
     print;
   }
